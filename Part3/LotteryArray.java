39b7e5cc92f284ae24dd1a72cfdc2703a0f49bee
newmlee <matthewlee0725@gmail.com>
2014-03-16 23:15:21 +0000

    多维数组

---
+++ Part3/LotteryArray.java
@@ -0,0 +1,28 @@
+public class LotteryArray {
+    public static void main(String[] args) {
+        final int NMAX = 10;
+
+        int[][] odds = new int[NMAX + 1][];
+        for (int n = 0; n <= NMAX; n++) {
+            odds[n] = new int[n + 1];
+        }
+
+        for (int n = 0; n < odds.length; n++) {
+            for (int k = 0; k < odds[n].length; k++) {
+                int lotteryOdds = 1;
+                for (int i = 1; i <= k; i++) {
+                    lotteryOdds = lotteryOdds * (n - i + 1) / i;
+                }
+                odds[n][k] = lotteryOdds;
+            }
+        }
+
+        for (int[] row : odds) {
+            for (int odd : row) {
+                System.out.printf("%4d", odd);
+            }
+            System.out.println();
+        }
+    }
+}
+
