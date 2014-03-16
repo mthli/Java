074f83dddc3ce9cf0d2a7399c33a439c2f6cd3d1
newmlee <matthewlee0725@gmail.com>
2014-03-16 23:14:56 +0000

    数组排序

---
+++ Part3/LotteryDrawing.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LotteryDrawing {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many numbers do you need to draw? ");
+        int k = in.nextInt();
+
+        System.out.print("What is the highest number you can draw? ");
+        int n = in.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            int r = (int)(Math.random() * n);
+            result[i] = numbers[r];
+            numbers[r] = numbers[n - 1];
+            n--;
+        }
+
+        Arrays.sort(result);
+        System.out.println("Bet the following combination. It'll make you rich!");
+        for (int r : result) {
+            System.out.println(r);
+        }
+    }
+}
+
