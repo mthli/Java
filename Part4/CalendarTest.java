cf6a8c5681d3d421641c3a8c605f6c5df2b00fe1
newmlee <matthewlee0725@gmail.com>
2014-04-23 08:41:18 +0000

    添加Part4/CalendarTest.java和Part4/EmployeeTest.java的注释

--- Part4/CalendarTest.java
+++ Part4/CalendarTest.java
@@ -1,6 +1,12 @@
 import java.text.DateFormatSymbols;
 import java.util.*;
 
+
+/* 这里需要提醒一下：
+ *     在一个Java源文件里面，
+ *     只能有一个公有类，
+ *     但是可以有任意数目的非公有类（非公有类!=私有类）
+ */
 public class CalendarTest {
     public static void main(String[] args) {
         GregorianCalendar d = new GregorianCalendar();
--- Part4/EmployeeTest.java
+++ Part4/EmployeeTest.java
@@ -21,11 +21,20 @@ public class EmployeeTest {
     }
 }
 
+/* 如前CalendarTest.java开头的注释所述，
+ * 这里的Employee就是一个非公有类，
+ * 但它不是一个私有类
+ */
 class Employee {
     private String name;
     private double salary;
     private Date hireDay;
 
+    /* 下面这段代码块是一个构造器，
+     * 顾名思义，构造器是用来构造一个新的对象实例的，
+     * 需要注意的是：
+     *     构造器的名字应该与所属类的名字相同
+     */
     public Employee(String n, double s, int year, int month, int day) {
         name = n;
         salary = s;
@@ -33,6 +42,11 @@ class Employee {
         hireDay = calendar.getTime();
     }
 
+    /* 以下的几处代码块是一些访问器和更改器，
+     * 通常的习惯是，
+     * 在访问器方法名前面加上前缀get，
+     * 而在更改器方法前面加上前缀set
+     */
     public String getName() {
         return name;
     }
