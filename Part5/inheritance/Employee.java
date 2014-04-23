e2202b5a06bb310930c7837fb803f552743adcac
newmlee <matthewlee0725@gmail.com>
2014-04-23 16:00:05 +0000

    完成Part5/inheritance

--- Part5/inheritance/Employee.java
+++ Part5/inheritance/Employee.java
@@ -3,37 +3,31 @@ package inheritance;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class Employee
-{
+public class Employee {
    private String name;
    private double salary;
    private Date hireDay;
 
-   public Employee(String n, double s, int year, int month, int day)
-   {
+   public Employee(String n, double s, int year, int month, int day) {
       name = n;
       salary = s;
       GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
       hireDay = calendar.getTime();
    }
 
-   public String getName()
-   {
+   public String getName() {
       return name;
    }
 
-   public double getSalary()
-   {
+   public double getSalary() {
       return salary;
    }
 
-   public Date getHireDay()
-   {
+   public Date getHireDay() {
       return hireDay;
    }
 
-   public void raiseSalary(double byPercent)
-   {
+   public void raiseSalary(double byPercent) {
       double raise = salary * byPercent / 100;
       salary += raise;
    }
--- Part5/inheritance/Manager.java
+++ Part5/inheritance/Manager.java
@@ -1,31 +1,30 @@
 package inheritance;
 
-public class Manager extends Employee
-{
+/* Java中使用extends关键字来表示“继承”的概念，
+ * extends关键字表明正在构造的新类派生于一个已经存在的类；
+ * 需要注意的是：
+ *     在Java中，所有的继承都是公用继承，
+ *     而没有C++中的私有继承和保护继承
+ */
+public class Manager extends Employee {
    private double bonus;
 
-   /**
-    * @param n the employee's name
-    * @param s the salary
-    * @param year the hire year
-    * @param month the hire month
-    * @param day the hire day
+   /* 下面这段代码块使用了super来调用其父类（超类）的构造器，
+    * super是一个指示编译器调用超类方法的特殊关键字；
+    * 注意super并不是一个对象的引用，不能将super赋给另一个变量对象
     */
-   public Manager(String n, double s, int year, int month, int day)
-   {
+   public Manager(String n, double s, int year, int month, int day) {
       super(n, s, year, month, day);
       bonus = 0;
    }
 
-   public double getSalary()
-   {
+   /* 在这里对其超类（Employee）中的getSalary方法进行了重写（override） */
+   public double getSalary() {
       double baseSalary = super.getSalary();
       return baseSalary + bonus;
    }
 
-   public void setBonus(double b)
-   {
+   public void setBonus(double b) {
       bonus = b;
    }
 }
-
