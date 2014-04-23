17863d4dea9001d8ef562b59e50596adc96bd197
newmlee <matthewlee0725@gmail.com>
2014-04-23 09:31:27 +0000

    添加Part4/ConstructorTest.java注释

--- Part4/ConstructorTest.java
+++ Part4/ConstructorTest.java
@@ -3,6 +3,11 @@ import java.util.*;
 public class ConstructorTest {
     public static void main(String[] args) {
         Emplogee[] staff = new Emplogee[3];
+        
+        /* 这里利用到了Java中的“重载”进行初始化，
+         * 当然在这里就不能像C++中叫“函数重载”了，
+         * 在Java中，应该叫做“方法重载”
+         */
         staff[0] = new Emplogee("Harry", 40000);
         staff[1] = new Emplogee(60000);
         staff[2] = new Emplogee();
@@ -24,16 +29,22 @@ class Emplogee {
     private String name = "";
     private double salary;
 
+    /* 下面的代码块用到的是“静态初始化块”
+     * 当对类的静态域进行初始化的代码比较复杂的时候，
+     * 可以考虑使用“静态初始化块”进行初始化
+     */
     static {
         Random generator = new Random();
         nextId = generator.nextInt(10000);
     }
 
+    /* 这里用到的是普通的初始化块 */
     {
         id = nextId;
         nextId++;
     }
 
+    /* 下面三个构造器代码块体现了所谓的“方法重载” */
     public Emplogee(String n, double s) {
         name = n;
         salary = s;
@@ -58,4 +69,3 @@ class Emplogee {
         return id;
     }
 }
-
